481af094c76e00f523b41eaa2b88ea40cf6f995b
pravhatray <pravhatray@gmail.com>
2022-05-02 16:25:14 +0530

    del ddone

--- u2c4 template 2/favourites.js
+++ u2c4 template 2/favourites.js
@@ -9,7 +9,7 @@ function display(list)
 {
     
     document.querySelector("tbody").innerHTML="";
-    list.forEach(function(element){
+    list.forEach(function(element,index){
 
 
         let tr=document.createElement("tr");
@@ -33,37 +33,19 @@ function display(list)
         td6.innerText="Delete";
         td6.style.color="red";
         td6.style.cursor="pointer";
-        // td6.addEventListener("click", function(){
-        //     remoeve(element,i);
-        // })
-
-        td6.addEventListener("click", function(event){
-            dele(element.ddate)
+        td6.addEventListener("click" , function() {
+            deleteFunction(element,index)
         })
+    
+       
         tr.append(td1,td2,td3,td4,td5,td6);
         document.querySelector("tbody").append(tr);
 
     })
 }
 
-function dele(date){
-    f=f.filter(function(element){
-        return element.ddate!=date;
-    })
-
-    display(f)
-    localStorage.setItem("dee",JSON.stringify(f))
-
+function  deleteFunction(element ,index){
+    f.splice(index,1)
+    localStorage.setItem("favourites",JSON.stringify(f));
+    window.location.reload()
 }
-// function remoeve(element,i){
-// f.splice(i,1);
-// localStorage.setItem("favourites" , JSON.stringify(f));
-// window.location.reload();
-// }
-
-// function del(element,index){
-    
-// f.splice(element,1);
-// localStorage.setItem("favo", JSON.stringify(f));
-// window.location.reload();
-// }
